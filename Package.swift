559de7a5003805a26e89797e26d50fdb7d19e783
Allen <jjl13142008@126.com>
2020-12-02 16:48:29 +0800

    1）update

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
 import PackageDescription
 
 let package = Package(
-  name: "Runes"
+  name: "Runes",
+  products: [
+    .library(name: "Runes", targets: ["Runes"])
+  ],
+  dependencies: [
+    .package(url: "git@github.com:typelift/SwiftCheck.git", from: "0.12.0")
+  ],
+  targets: [
+    .target(name: "Runes"),
+    .testTarget(
+      name: "RunesTests",
+      dependencies: [
+        "Runes",
+        "SwiftCheck",
+      ]
+    ),
+  ]
 )
